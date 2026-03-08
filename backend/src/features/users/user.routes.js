a24b93dd0074ae47f5fc041f7a08be45c6032666
AhmedSocrates <ahmedkinglion@gmail.com>
2026-03-08 04:50:12 +0400

    Ticket 4 is almost done, waiting for testing, I added a logic for streak calculations, also linked the inventory with the streak directly so that if the user missed a day and he has streak pauser, the app will use it directly

--- backend/src/features/users/user.routes.js
+++ backend/src/features/users/user.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import {getMyProfile,updateProfile,updateSettings} from './user.controller.js';
 import {protectRoute} from '../../middlewares/auth.middlewares.js';
-
+import { streakEngine } from '../../middlewares/streak.middleware.js';
 const router = express.Router();
-router.get('/me', protectRoute, getMyProfile);
-router.put('/profile', protectRoute, updateProfile);
-router.put('/settings', protectRoute, updateSettings);
+router.get('/me', protectRoute, streakEngine, getMyProfile);
+router.put('/profile', protectRoute, streakEngine, updateProfile);
+router.put('/settings', protectRoute, streakEngine, updateSettings);
 export default router;
